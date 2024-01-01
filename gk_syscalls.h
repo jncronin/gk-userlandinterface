27f5594c298225911ae6ec5ed0a6d56c370783dd
John Cronin <jncronin@tysos.org>
2024-01-01 13:41:54 +0000

    Initial commit

---
+++ gk_syscalls.h
@@ -0,0 +1,22 @@
+#ifndef GKSYSCALLS_H
+#define GKSYSCALLS_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+struct gk_syscalls
+{
+
+
+};
+
+const struct gk_syscalls *gk_get_syscalls();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
