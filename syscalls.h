689e2b4b5f1db1e4895e95524a34b943da7c3891
John Cronin <jncronin@tysos.org>
2024-02-06 12:05:28 +0000

    new syscall interface

--- syscalls.h
+++ syscalls.h
@@ -8,7 +8,56 @@ enum syscall_no
 
     FlipFrameBuffer,
     SetFrameBuffer,
-    GetFrameBuffer
+    GetFrameBuffer,
+
+    /* newlib libgloss */
+    __syscall_close,
+    __syscall_execve,
+    __syscall_exit,
+    __syscall_fork,
+    __syscall_fstat,
+    __syscall_getpid,
+    __syscall_gettimeofday,
+    __syscall_isatty,
+    __syscall_kill,
+    __syscall_link,
+    __syscall_lseek,
+    __syscall_open,
+    __syscall_read,
+    __syscall_sbrk,
+    __syscall_stat,
+    __syscall_unlink,
+    __syscall_wait,
+    __syscall_write
+};
+
+/* parameters for above */
+struct __syscall_execve_params
+{
+    char *name;
+	char **argv;
+	char **env;
+};
+
+struct __syscall_lseek_params
+{
+    int file;
+    int offset;
+    int whence;
+};
+
+struct __syscall_open_params
+{
+    const char *name;
+    int flags;
+    int mode;
+};
+
+struct __syscall_read_params
+{
+    int file;
+    char *ptr;
+    int len;
 };
 
 enum syscall_framebuffer_pixelformat
@@ -32,7 +81,7 @@ extern "C"
 #define NULL ((void*)0)
 #endif
 
-static inline void __syscall(syscall_no sno, void *r1, void *r2, void *r3)
+static inline void __syscall(enum syscall_no sno, void *r1, void *r2, void *r3)
 {
     register unsigned int _sno asm("r0") = sno;
     register void *_r1 asm("r1") = r1;
@@ -66,7 +115,7 @@ static inline void *__syscall_GetFrameBuffer()
     return ret;
 }
 
-static inline void __syscall_SetFrameBuffer(void *b0, void *b1, syscall_framebuffer_pixelformat pf)
+static inline void __syscall_SetFrameBuffer(void *b0, void *b1, enum syscall_framebuffer_pixelformat pf)
 {
     __syscall(SetFrameBuffer, b0, b1, (void*)pf);
 }
