583abdc5a7e668ad7e3a14e1e20d7aff14ce764d
John Cronin <jncronin@tysos.org>
2024-03-31 18:16:49 +0100

    add DT_DIR et al

--- _sys_dirent.h
+++ _sys_dirent.h
@@ -12,5 +12,13 @@ struct dirent
     char                d_name[256];
 };
 
+#define DT_UNKNOWN      0
+#define DT_REG          1
+#define DT_DIR          2
+#define DT_FIFO         5
+#define DT_SOCK         6
+#define DT_CHR          3
+#define DT_BLK          4
+#define DT_LNK          7
 
 #endif
