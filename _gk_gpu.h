f019a6808b8eca673770cc9264959a24811cdf1a
John Cronin <jncronin@tysos.org>
2025-11-30 12:09:23 +0000

    add extra pixel formats

--- _gk_gpu.h
+++ _gk_gpu.h
@@ -15,8 +15,10 @@
 #define GK_PIXELFORMAT_L8           8
 #define GK_PIXELFORMAT_A8L8         9
 #define GK_PIXELFORMAT_A4L4         10
+#define GK_PIXELFORMAT_ARBG1555     11
+#define GK_PIXELFORMAT_ARGB4444     12
 
-#define GK_PIXELFORMAT_MAX          10
+#define GK_PIXELFORMAT_MAX          12
 #else
 #define GK_PIXELFORMAT_ARGB8888     0
 #define GK_PIXELFORMAT_RGB888       1
