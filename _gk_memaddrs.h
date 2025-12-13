0d53c76e398a49a4b3db46ab078ed9d12a243b6e
John Cronin <jncronin@tysos.org>
2025-12-13 11:06:50 +0000

    add gk_kernel_info struct

--- _gk_memaddrs.h
+++ _gk_memaddrs.h
@@ -9,12 +9,24 @@
 #define GK_SCR_L1_B2                (GK_PROCESS_INTERFACE_START + 0x2000000ULL)
 #define GK_ROINFO_PAGE              (GK_PROCESS_INTERFACE_START + 0x3000000ULL)
 #define GK_TIM3                     (GK_PROCESS_INTERFACE_START + 0x3010000ULL)
+#define GK_KERNEL_INFO_PAGE         (GK_PROCESS_INTERFACE_START + 0x3020000ULL)
 
 #define GK_CUR_S_ADDRESS            (GK_ROINFO_PAGE + 0xfe00ULL)
 #define GK_TIM_PRECISION_NS_ADDRESS (GK_ROINFO_PAGE + 0xfe08ULL)
 #define GK_TOFFSET_ADDRESS          (GK_ROINFO_PAGE + 0xfe10ULL)
 #define GK_JOYSTICK_ADDRESS         (GK_ROINFO_PAGE + 0xfe80ULL)
 #define GK_TILT_ADDRESS             (GK_ROINFO_PAGE + 0xff00ULL)
+
+struct gk_kernel_info
+{
+    unsigned int gk_ver;
+    unsigned int max_screen_width;
+    unsigned int max_screen_height;
+    unsigned int ncores;
+    unsigned int page_size;
+} __attribute__((packed));
+
+#define GK_KERNEL_INFO (const gk_kernel_info *)(GK_KERNEL_INFO_PAGE);
 
 #else
 #define GK_TLS_POINTER_ADDRESS      0x58004518U
